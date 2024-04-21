0d0a77914073b0a478539731aecb776ef45c62f0
JavFuertes <j.fuertesguadarrama@student.tudelft.nl>
2024-04-21 23:49:18 +0200

    logic fix

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ images = load_images(folder_path= folder_path, desired_size=(1000, 200))
 cartoon_images = [apply_cartoon_effect(pixelate_image(img, pixel_size = 8), levels= 7) for img in images]
 
 all_frames = []
-for i in range(len(cartoon_images) - 1):
-    all_frames += create_transition_frames(cartoon_images[i], cartoon_images[i+1], steps= 20)
-    all_frames += create_steady_frames(cartoon_images[i+1], steps=20) 
+for i in range(len(cartoon_images)-1):
+    all_frames += create_transition_frames(cartoon_images[i], cartoon_images[i+1], steps= 8)
+    all_frames += create_steady_frames(cartoon_images[i+1], steps=14) 
 
-all_frames += create_transition_frames(cartoon_images[-1], cartoon_images[0], steps= 20)
+all_frames += create_transition_frames(cartoon_images[-1], cartoon_images[0], steps= 8)
+all_frames += create_steady_frames(cartoon_images[0], steps=14) 
+
 all_frames[0].save(name, save_all=True, append_images=all_frames[1:], optimize=False, duration= 150, loop=0)
